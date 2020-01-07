2da0db22e6a6e1e2723dddcda5811e3d3d95b78f
Maurizio Tonelli <mauriziotonelli@hotmail.com>
2020-01-07 14:02:40 -0600

    Add Token object. Add expression, term, primary

--- main.js
+++ main.js
@@ -1,20 +1,54 @@
 //Add tokenStream
 //Add token
 
-function primary(){
-
+function getToken(obj){
+    let val = '';
+    if(obj.value == '') return false;
+    if((obj.value[0] >='0' && obj.value[0]<='9')||obj.value[0] == '.'){
+        while((obj.value[0] >='0' && obj.value[0]<='9')||obj.value[0] == '.'){
+            val+=obj.value[0];
+            obj.value=obj.value.slice(1);
+        }
+        return {value: val, type: 'int'};
+    }else{
+        val =obj.value[0];
+        obj.value=obj.value.slice(1);
+        return {value: val, type: 'operator'}; 
+    }
 }
 
-function term(){
-
+function primary(obj){
+    let token = getToken(obj);
+    switch(token.type){
+        case 'int':
+            return token.value;
+            break;
+        case 'operator':
+            if(token.value == '('){
+                let left = expression(obj);
+                token = getToken(obj);
+                if(token.value != ')'){
+                    return 'error';
+                }
+                return left;
+            }else{
+                return 'error';
+            }
+    }
 }
 
-function expression(){
+function term(obj){
+    let left = primary(obj);
+}
 
+function expression(obj){
+    let left = term(obj);
 }
 
 function calculate(str){
-    return '0';
+
+    let exp ={value: str};
+    return expression(exp);
 }
 
 function writeToDisplay(e){
